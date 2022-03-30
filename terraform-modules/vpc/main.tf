375dc068312a0c6184ad57bfccbe72ab2988cf66
Paul Schwarzenberger <paul@celidor.net>
2022-03-30 16:41:18 +0100

    check Internet connectivity before trying to install Apache

--- terraform-modules/vpc/main.tf
+++ terraform-modules/vpc/main.tf
@@ -231,6 +231,12 @@ resource "aws_security_group" "secgroup_group" {
   }
 
   egress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
